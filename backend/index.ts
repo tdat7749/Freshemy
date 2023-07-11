5b4fd0ea87bfb7a599b01b4641c6f2b8b6c4db15
Your Name <you@example.com>
2023-07-11 12:45:05 +0700

    folder_dev

--- backend/index.ts
+++ backend/index.ts
@@ -14,48 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 
 const port: number = 3000;
 
-app.get("/", (_req, res: Response) => {
-    res.send(`Server is running on port: ${port}`);
-});
-
-app.get("/api/todos", async (req: Request, res: Response) => {
-    try {
-        //@ts-ignore
-        const allUsers = await prisma.todo.findMany();
-        return res.json({
-            success: true,
-            data: allUsers,
-        });
-    } catch (error) {
-        return res.json({
-            success: false,
-            message: error,
-        });
-    }
-});
-
-app.post("/api/todos", async (req: Request, res: Response) => {
-    try {
-        const { title, description, completed } = req.body;
-        //@ts-ignore
-        const newTodo = await prisma.todo.create({
-            data: {
-                title,
-                description,
-                completed,
-            },
-        });
-
-        return res.json({
-            success: true,
-            data: newTodo,
-        });
-    } catch (error) {
-        return res.json({
-            success: false,
-            message: error,
-        });
-    }
+app.get("/a", (_req, res: Response) => {
+    res.send(`hello`);
 });
 
 app.listen(port, () => {
