e1283c49522f34ec1e9df094820f067a91fbcfac
tdat7749 <hyperiondat@gmail.com>
2023-07-12 16:45:59 +0700

    Login: Implement UI and API

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Request, Response } from "express";
 import { loginSchema } from "../validations/auth";
 import service from "../services";
@@ -33,30 +32,3 @@ class AuthController {
 }
 
 export default AuthController;
-=======
-import { Request, Response } from 'express'
-import { loginSchema } from 'src/validations/auth'
-import service from '../services'
-
-
-class AuthController {
-    async login(req: Request, res: Response) {
-        const { error: errorValidate } = loginSchema.validate(req.body)
-
-        if (errorValidate) {
-            //Return lỗi
-        }
-
-        const response = await service.AuthService.login(req)
-
-        //return res.status().json(isResponse)
-    }
-
-    async refreshToken(req: Request, res: Response) { //return type Promise<Response>
-        const response = await service.AuthService.refreshToken(req)
-        //return res.status().json(response)
-    }
-}
-
-
->>>>>>> 1101f896025d76ca31b1cf07a66bb59236713c79
--- backend/src/types/decodeToken.d.ts
+++ backend/src/types/decodeToken.d.ts
@@ -1,10 +1,5 @@
 import { JwtPayload } from "jsonwebtoken";
 
 export interface MyJwtPayload extends JwtPayload {
-<<<<<<< HEAD
     user_id?: number;
 }
-=======
-    user_id: number
-}
->>>>>>> 1101f896025d76ca31b1cf07a66bb59236713c79
