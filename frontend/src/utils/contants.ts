1eb9c375da91956534eee102a49fba9071363040
tdat7749 <hyperiondat@gmail.com>
2023-07-17 11:36:08 +0700

    add utils for backend

--- frontend/src/utils/contants.ts
+++ frontend/src/utils/contants.ts
@@ -1,1 +1,4 @@
 export const MESSAGE_ERROR_PASSWORD_WRONG = "Wrong password";
+export const MESSAGE_ERROR_EMAIL_INVALID = "Invalid email";
+export const MESSAGE_ERROR_EMAIL_REQUIRED = "Email is required";
+export const MESSAGE_ERROR_PASSWORD_REQUIRED = "Password is required";
