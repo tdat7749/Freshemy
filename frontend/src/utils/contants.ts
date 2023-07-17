4750eaf5f37fdb7607e1d9324741c6bbc020cc11
Vinh Phuc <vinhphuccse@gmail.com>
2023-07-17 11:42:35 +0700

    Forgot pw: change message to constant

--- frontend/src/utils/contants.ts
+++ frontend/src/utils/contants.ts
@@ -1,1 +1,3 @@
 export const MESSAGE_ERROR_PASSWORD_WRONG = "Wrong password";
+export const MESSAGE_ERROR_EMAIL_INVALID = "Invalid email";
+export const MESSAGE_ERROR_EMAIL_REQUIRED = "Email is required";
