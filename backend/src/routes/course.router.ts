9c7353a57791007c231a66f454c661af53c3ed5d
Dương Song <duongsong9a4@gmail.com>
2023-07-19 17:24:52 +0700

    Edit course: fix slug structure

--- backend/src/routes/course.router.ts
+++ backend/src/routes/course.router.ts
@@ -6,14 +6,12 @@ import { uploadFile } from "../middlewares/multer";
 const courseRouter: Router = Router();
 
 courseRouter.post("/", isLogin, uploadFile, controllers.courseController.createCourse);
-courseRouter.get("/:slug", controllers.courseController.getCourseDetail);
 courseRouter.post("/registration",isLogin,controllers.courseController.registerCourse)
 courseRouter.delete("/unsubcribe",isLogin,controllers.courseController.unsubcribeCourse)
 courseRouter.put("/change-information",isLogin, controllers.courseController.editCourse);
 courseRouter.get("/search-my-courses", isLogin, controllers.courseController.searchMyCourses);
-
 courseRouter.delete("/:id", isLogin, controllers.courseController.deleteMyCourse);
-
 courseRouter.post("/", isLogin, uploadFile, controllers.courseController.createCourse);
 courseRouter.patch("/change-thumbnail",isLogin,uploadFile, controllers.courseController.editThumbnail);
+courseRouter.get("/:slug", controllers.courseController.getCourseDetail);
 export default courseRouter;
