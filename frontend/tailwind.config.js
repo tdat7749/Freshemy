ffed452de3dd9b7ed3944cf5b06ec72ea9ec3672
Vinh Phuc <vinhphuccse@gmail.com>
2023-07-21 17:56:37 +0700

    Npm-lib: settup daisy UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,36 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    screens: {
-      'mobile': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'tablet': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        screens: {
+            mobile: "640px",
+            tablet: "768px",
+            laptop: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
+        extend: {
+            colors: {
+                background: "#F8FFF8",
+                primary: "#F7F1DD",
+                error: "#FF0000",
+                switch: "#033700",
+                bgForm: "#F7F1DD",
+                text: "#212B27",
+                backgroundHover: "#FFCF90",
+                success: "#28a745",
+            },
+        },
     },
-    extend: {
-      colors:{
-        'background': '#F8FFF8',
-        'primary':'#F7F1DD',
-        'error':'#FF0000',
-        'switch':'#033700',
-        'bgForm': '#F7F1DD',
-        'text':'#212B27',
-        'backgroundHover':'#FFCF90',
-        'success':'#28a745'
-      },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                // mytheme: {
+                //     primary: "#555555", 
+                //     secondary: "#ff0000",
+                //     "base-100": "#fff",
+                //     "base-content": "#5c5c5c",
+                //     accent: "#FAFAFA",
+                //     neutral: "#000",
+                // },
+            },
+        ],
     },
-  },
-  plugins: [],
-}
-
+};
