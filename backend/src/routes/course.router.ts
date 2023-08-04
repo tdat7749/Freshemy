bba271a54c3e0c853f5df6bab6290a4960262dc5
tdat7749 <hyperiondat@gmail.com>
2023-08-04 08:59:05 +0700

    refactor-api-endpoint: fix bug

--- backend/src/routes/course.router.ts
+++ backend/src/routes/course.router.ts
@@ -16,7 +16,7 @@ courseRouter.delete("/unsubcribe", isLogin, controllers.courseController.unsubcr
 courseRouter.get("/:slug/ratings", controllers.courseController.getListRatingsOfCourseBySlug);
 courseRouter.get("/top-10", controllers.courseController.getTop10Courses);
 courseRouter.get("/my-courses", isLogin, controllers.courseController.searchMyCourses);
-courseRouter.get("/enrolled", isLogin, controllers.courseController.searchEnrolledCourses);
+courseRouter.get("/enrolled-courses", isLogin, controllers.courseController.searchEnrolledCourses);
 courseRouter.get("/all-courses", controllers.courseController.getAllCourses);
 
 courseRouter.get("/:course_id/section", isLogin, controllers.sectionController.getAllSectionByCourseId);
